492f8e614e2543fb0f8574728482430f8efc0857
Silvio Ronaldo <silvio.ronaldo77@gmail.com>
2022-01-21 01:29:17 -0300

    Create main header

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
 
-import { Container, Title } from './styles';
+import { Container, Header, TotalCars } from './styles';
+
+import Logo from '../../assets/logo.svg';
 
 export function Home() {
   return (
     <Container>
-      <Title>Home</Title>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      <Header>
+        <Logo width={RFValue(108)} height={RFValue(12)} />
+
+        <TotalCars>Total de 14 carros</TotalCars>
+      </Header>
     </Container>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,13 +1,26 @@
 import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
 
   background-color: ${({ theme }) => theme.colors.background_primary};
 `;
 
-export const Title = styled.Text`
-  font-family: ${({ theme }) => theme.fonts.secondary_600};
+export const Header = styled.View`
+  width: 100%;
+  height: ${RFValue(113)}px;
+  background-color: ${({ theme }) => theme.colors.header};
+
+  flex-direction: row;
+  align-items: flex-end;
+  justify-content: space-between;
+
+  padding: 32px 24px;
+`;
+
+export const TotalCars = styled.Text`
+  color: ${({ theme }) => theme.colors.text};
+  font-size: ${RFValue(15)}px;
+  font-family: ${({ theme }) => theme.fonts.primary_400};
 `;
